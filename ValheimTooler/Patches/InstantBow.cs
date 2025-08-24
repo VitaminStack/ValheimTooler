0d81958b512167b7f0f400596eacd76ffa27fa5c
SozialHilfe <109233840+VitaminStack@users.noreply.github.com>
2025-08-24 16:54:42 +0200

    Fix Instant Bow patch to target bow draw method

--- ValheimTooler/Patches/InstantBow.cs
+++ ValheimTooler/Patches/InstantBow.cs
@@ -5,10 +5,10 @@ using ValheimTooler.Utils;
 
 namespace ValheimTooler.Patches
 {
-    [HarmonyPatch(typeof(Player), "UpdateAttack", new Type[] { typeof(float) })]
+    [HarmonyPatch(typeof(Player), "UpdateAttackBowDraw")]
     class InstantBow
     {
-        private static void Prefix(Player __instance)
+        private static void Postfix(Player __instance)
         {
             if (!PlayerHacks.s_instantBow || __instance != Player.m_localPlayer)
             {
@@ -18,8 +18,8 @@ namespace ValheimTooler.Patches
             Attack currentAttack = __instance.GetFieldValue<Attack>("m_currentAttack");
             if (currentAttack != null && currentAttack.m_bowDraw)
             {
-                __instance.SetFieldValue("m_attackDrawPercentage", 1f);
                 __instance.SetFieldValue("m_attackDrawTime", currentAttack.m_drawDurationMin);
+                __instance.SetFieldValue("m_attackDrawPercentage", 1f);
             }
         }
     }
